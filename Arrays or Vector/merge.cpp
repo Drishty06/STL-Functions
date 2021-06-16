e2114af1e7c6e77d89f5d34e27959c30cef101cb
Suketu Piprotar <72180702+suketupiprotar@users.noreply.github.com>
2021-06-16 22:33:45 +0530

    Create merge.cpp

---
+++ Arrays or Vector/merge.cpp
@@ -0,0 +1,38 @@
+// used for : merge two sorted containers and stores in new container in sorted order
+// syntax:  merge(begin1,end1,begin2,end2,begin3)
+// header file: #include <algorithm>
+
+// code snippet:
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+ // Initializing 1st vector
+ vector<int> v1 = {1, 3, 4, 5, 20, 30};
+      
+ // Initializing 2nd vector
+ vector<int> v2 = {1, 5, 6, 7, 25, 30};
+      
+ // Declaring resultant vector
+ // for mergeing
+ vector<int> v3(12);
+      
+ // Using merge() to merge vectors v1 and v2
+ // and storing result in v3
+ merge(v1.begin(), v1.end(), v2.begin(), 
+                   v2.end(), v3.begin());
+      
+ // Displaying resultant container
+ cout << "The new container after merging is :\n";
+ for (int &x : v3) 
+     cout << x << " ";
+ cout << endl;
+  
+  return 0;
+}
+
+// output: 
+// The new container after merging is :
+// 1 1 3 4 5 5 6 7 20 25 30 30 
