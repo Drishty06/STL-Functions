a0f7a2d8fe4469c1ae7b9c9609e403d1accd5917
Suketu Piprotar <72180702+suketupiprotar@users.noreply.github.com>
2021-07-28 21:49:59 +0530

    Update Execution time.cpp

--- Execution time.cpp
+++ Execution time.cpp
@@ -13,7 +13,7 @@ long fibonacci(unsigned n)
 int main()
 {
     auto start = std::chrono::steady_clock::now();  // start from here(from the where you want to show execution time)
-    std::cout << "f(2) = " << fibonacci(42) << '\n'; 
+    std::cout << "f(2) = " << fibonacci(2) << '\n'; 
     auto end = std::chrono::steady_clock::now();   // end here(at the end of function)
     std::chrono::duration<double> elapsed_seconds = end-start; // this is calculate duration of execution time of the function
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n"; // print the exicution time
